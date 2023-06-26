cadf1bc432a4b6652cf1d43df80cc3be5461183e
Brandon Lim <brandonlim10p@icloud.com>
2023-06-26 14:29:55 -0700

    login/logout

--- til/settings.py
+++ til/settings.py
@@ -142,7 +142,7 @@ ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 2
 AUTHENTICATION_BACKENDS = (
-    "django.contrib.auth.backends.ModelsBackend",
+    "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend"
 )
 
